144c88c2f6a2063d518ab8ff225577c7ba7422c5
aemann01 <aemann01@ou.edu>
2019-03-12 11:54:53 -0700

    new rep for ITS project

---
+++ demultiplex.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Script for demultiplexing ITS data, gather some simple summary statistics on the raw reads
+# Use: ./demultiplex.sh
+###################
+#Demultiplexing
+###################
+#after logging onto deep thought
+cd /home/user/mann
+bcl2fastq -R /media/Synology_Data/Seq_Run_Data/LMAMR51_Mar_2019/ -o ITSfastq --use-bases-mask Y*,I*,I*,Y* --sample-sheet sampleSheet.csv
+#get file name and raw count
+ls *gz > file
+ls *gz | while read line; do zcat $line | wc -l ; done >> raw
+awk '{print $1/4}' raw > reads
+paste file reads > readcount.txt
+rm file raw reads
+
+#get summary stats of raw reads
+average=$(awk '{ sum += $2; n++ } END { if (n > 0) print sum / n; }' readcount.txt)
+median=$(awk '{print $2}' readcount.txt | sort -n | awk ' { a[i++]=$1; } END { x=int((i+1)/2); if (x < (i+1)/2) print (a[x-1]+a[x])/2; else print a[x-1]; }')
+max=$(awk '{print $2}' readcount.txt | sort -n | tail -n1)
+min=$(awk '{print $2}' readcount.txt | sort -n | head -n1)
+echo "Average:" $average >> readcount.stats.txt
+echo "Median:" $median >> readcount.stats.txt
+echo "Minimum read count:" $min >> readcount.stats.txt
+echo "Maximum read count:" $max >> readcount.stats.txt
---
+++ preprocessing.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Quality filter, merge, and process raw data
+# Use: ./
+###################
+#Demultiplexing
+###################
